b93e41fe18dada0614a45c2aa5c6f35b7d99e518
Gautham Sakthi <gauthamroses@gmail.com>
2022-01-30 18:53:02 +0530

    Code Refactored

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
+import React from "react";
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import CardList from "./Components/card-list/CardList";
-import Searchbox from "./Components/searchbox/Searchbox";
+import Monster from "./Pages/Monster";
 
 function App() {
-  const [state, setState] = useState(() => {
-    return { monsters: [], error: "" };
-  });
-  const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          throw new Error("Something went wrong");
-        }
-      })
-      .then((data) => {
-        return setState({ monsters: data });
-      })
-      .catch((err) => {
-        return setState({ error: err });
-      });
-  }, []);
-
-  const { monsters } = state;
-  const filteredMonsters = monsters.filter((monster) =>
-    monster.name.toLowerCase().trim().includes(search.toLowerCase())
-  );
   return (
-    <div className="App">
-      <Searchbox
-        setSearch={setSearch}
-        search={search}
-        placeholder="search monsters"
-      />
-      <CardList monsters={filteredMonsters}></CardList>
+    <div>
+      <Monster />
     </div>
   );
 }
---
+++ src/Pages/Monster.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import CardList from "../Components/card-list/CardList";
+import Searchbox from "../Components/searchbox/Searchbox";
+import "./MonsterStyle.css";
+
+function Monster() {
+  const [state, setState] = useState(() => {
+    return { monsters: [], error: "" };
+  });
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Something went wrong");
+        }
+      })
+      .then((data) => {
+        return setState({ monsters: data });
+      })
+      .catch((err) => {
+        return setState({ error: err });
+      });
+  }, []);
+
+  const { monsters } = state;
+  const filteredMonsters = monsters.filter((monster) =>
+    monster.name.toLowerCase().trim().includes(search.toLowerCase())
+  );
+  return (
+    <div className="App">
+      <h1>Monsters World Search</h1>
+      <Searchbox
+        setSearch={setSearch}
+        search={search}
+        placeholder="Search monsters"
+      />
+      <CardList monsters={filteredMonsters}></CardList>
+    </div>
+  );
+}
+
+export default Monster;
