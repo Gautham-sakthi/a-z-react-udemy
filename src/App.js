0997a852970815b9359a253041acdd1f15ac2aac
Gautham Sakthi <gauthamroses@gmail.com>
2022-01-31 01:50:35 +0530

    Shopping cart skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
-import Monster from "./Pages/Monster";
+import Homepage from "./Components/Cart/Homepage";
+//import Monster from "./Pages/Monster";
 
 function App() {
   return (
     <div>
-      <Monster />
+      {/* <Monster /> */}
+      <Homepage />
     </div>
   );
 }
